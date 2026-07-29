67efa13658dc96d89e9c9c9d30b0debd9588c538
varshagawali1008 <varshagawali2004@gmail.com>
2026-07-29 02:41:48 -0700

    First Commit

---
+++ backend/app.py
@@ -0,0 +1,9 @@
+print("Welcome to My Python App!")
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum =", num1 + num2)
